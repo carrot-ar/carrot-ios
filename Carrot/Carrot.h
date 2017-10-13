9e76b270a38a52e8921d0ecd45667895c0bae6f6
gonzalonunez <gonzi@tcpmiami.com>
2017-10-13 11:17:46 -0400

    Created project, added SocketRocket dependency

---
+++ Carrot/Carrot.h
@@ -0,0 +1,19 @@
+//
+//  Carrot.h
+//  Carrot
+//
+//  Created by Gonzalo Nunez on 10/13/17.
+//  Copyright © 2017 carrot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Carrot.
+FOUNDATION_EXPORT double CarrotVersionNumber;
+
+//! Project version string for Carrot.
+FOUNDATION_EXPORT const unsigned char CarrotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carrot/PublicHeader.h>
+
+
